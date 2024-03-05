6b3ee5d059d878a57a76f3252069164252d03e76
Alex <fnelclsrn123@gmail.com>
2024-03-05 15:08:56 +0900

    refactor: Pawn 레코드에서 클래스로 수정 불변객체이면 안돼서 클래스로 수정

--- untitled/src/main/java/org/example/Pawn.java
+++ untitled/src/main/java/org/example/Pawn.java
@@ -1,4 +1,20 @@
 package org.example;
 
-public record Pawn(Color color) {
+public class Pawn {
+
+    public static final String BLACK = "black";
+    public static final String WHTIE = "white";
+
+    String color;
+
+    public Pawn(String color){
+        if(!color.equals(BLACK)&&!color.equals(WHTIE)){
+            throw new IllegalArgumentException("폰의 색은 하얀색과 검은색만 가능합니다.");
+        }
+        this.color =color;
+    }
+
+    public boolean verifyPawn(String color){
+     return this.color==color;
+    }
 }
