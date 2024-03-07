391bed1fb037b83eebcf354c87947c37e09a22b7
Alex <fnelclsrn123@gmail.com>
2024-03-07 13:58:29 +0900

    refactor: board 클래스 continue제거 중복 코드 제거를 위해서 if else 구조로 변경

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -10,28 +10,35 @@ import java.util.List;
 public class Board {
 
     private static List<List<Piece>> BOARD = intialize();
+    public static final int COUNT_OF_PIECES = 8;
+    public static final int ROW_OF_BLACKPAWN = 1;
+    public static final int ROW_OF_WHITEPAWN = 6;
+    public static final String BLACK = "black";
+    public static final String WHITE = "white";
+
 
     private static List<List<Piece>> intialize() {
         List<List<Piece>> board = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            if (i == 1) {
-                add(Pieces.BLACKPAWN, board);
-                continue;
+        for (int i = 0; i < COUNT_OF_PIECES; i++) {
+            if (i == ROW_OF_BLACKPAWN) {
+                add(BLACK, Pieces.BLACKPAWN, board);
+
+            } else if (i == ROW_OF_WHITEPAWN) {
+                add(WHITE, Pieces.WHITEPAWN, board);
+
+            } else {
+                add(WHITE, Pieces.NONE, board);
             }
-            if (i == 6) {
-                add(Pieces.WHITEPAWN, board);
-                continue;
-            }
-            add(Pieces.NONE, board);
         }
+
         return board;
     }
 
-    private static void add(Pieces pices, List<List<Piece>> board) {
+    private static void add(String color, Pieces piece, List<List<Piece>> board) {
         List<Piece> row = new ArrayList<>();
 
-        for (int j = 0; j < 8; j++) {
-            row.add(new Pawn(pices));
+        for (int j = 0; j < COUNT_OF_PIECES; j++) {
+            row.add(new Pawn(color, piece));
         }
 
         board.add(row);
