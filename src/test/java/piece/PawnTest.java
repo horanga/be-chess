918e5ca462e1e6fd3085eb7fabb97487eefbb348
Alex <fnelclsrn123@gmail.com>
2024-03-07 14:02:26 +0900

    refactor: pawn에 들어오는 색 예외처리 enum을 쓰지 않고 String으로 전달하기로 함 enum을 쓰면 타입 안전성을 얻을 수 있지만 null 체크를 해줘야 해서 큰 이점이 없다고 판단함 String에서 하얀색과 검은색만 받을 수 있도록 예외처리를 했음

--- src/test/java/piece/PawnTest.java
+++ src/test/java/piece/PawnTest.java
@@ -1,22 +1,46 @@
 package piece;
 
+import chess.Board;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PawnTest {
 
 
-    @DisplayName("하얀색, 검은색 폰을 생성한다.")
+    @DisplayName("하얀 폰을 생성한다.")
     @Test
     void test1() {
 
-        Pawn whitePawn = new Pawn(Pieces.WHITEPAWN);
+        Pawn whitePawn = new Pawn(Board.WHITE, Pieces.WHITEPAWN);
         assertThat(whitePawn.getColor()).isEqualTo("white");
+        assertThat(whitePawn.getLogo()).isEqualTo("p");
 
-        Pawn blackPawn = new Pawn(Pieces.BLACKPAWN);
+    }
+
+    @DisplayName("검은 폰을 생성한다.")
+    @Test
+    void test2() {
+        Pawn blackPawn = new Pawn(Board.BLACK, Pieces.BLACKPAWN);
         assertThat(blackPawn.getColor()).isEqualTo("black");
+        assertThat(blackPawn.getLogo()).isEqualTo("P");
+    }
+
+    @DisplayName("지정 색이 아니면 폰을 생성하지 않는다.")
+    @Test
+    void test3() {
+
+        List<Pawn> list = new ArrayList<>();
+
+        assertThatThrownBy(() -> {
+            new Pawn("blue", Pieces.NONE);
+        }).isInstanceOf(IllegalArgumentException.class);
+
     }
 
 }
